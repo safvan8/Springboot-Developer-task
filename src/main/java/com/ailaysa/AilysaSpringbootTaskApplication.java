5dc612bf7452e2fc209608f5eb2857e9fc758a23
Safvan <safvanpmcz@gmail.com>
2023-07-28 06:33:31 +0530

    initial commit

---
+++ src/main/java/com/ailaysa/AilysaSpringbootTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ailaysa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AilysaSpringbootTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AilysaSpringbootTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ailaysa/AilysaSpringbootTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailaysa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AilysaSpringbootTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
